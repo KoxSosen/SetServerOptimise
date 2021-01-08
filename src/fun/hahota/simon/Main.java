54c01e2d14d67ac6b2611f079722371332820da1
KoxSosen <whyconfused2@gmail.com>
2021-01-08 10:04:52 +0100

    get rid of spagetti

--- src/fun/hahota/simon/Main.java
+++ src/fun/hahota/simon/Main.java
@@ -20,6 +20,7 @@ public class Main extends JavaPlugin implements Listener {
 	public void onEnable() {
 		getLogger().info(ChatColor.GREEN + "SetServerOptimise is enabled!");
 		getLogger().info(ChatColor.GREEN + "Make sure to give the plugin a positive review if you enjoy it's features.");
+		getServer().getPluginManager().registerEvents(this, this);
 		
 		
 	}
@@ -29,33 +30,15 @@ public class Main extends JavaPlugin implements Listener {
 		getLogger().info(ChatColor.RED + "Goodbye!");
 	}
 			
-	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
-	if (cmd.getName().equalsIgnoreCase("optimise")) {
-		if (sender instanceof Player) {
-			Player player = (Player) sender;
-			if (player.hasPermission("optimise.use")) {
-				getServer().getPluginManager().registerEvents(this, this);
-			}
-		}
-	
-	 
-	}
-	return true;
-	
-	}
+
     	@EventHandler
     	public void onServerCommand(ServerCommandEvent event) {
     	  FileConfiguration bukkit = YamlConfiguration.loadConfiguration(new File(Bukkit.getServer().getWorldContainer(), "bukkit.yml"));
     	  if (event.getCommand().equalsIgnoreCase("optimise")) {
-    	    // Restarts server if server is set up for it.
+    		  // get spawn limits
     	    if (bukkit.getBoolean("spawn-limits")) {
-    	      Bukkit.getLogger().severe(ChatColor.translateAlternateColorCodes('&', "&8-" + getConfig().getString("MessageBeforeStopLoL")));
-    	      event.setCommand("restart");
-    	    } else {
-    	      // Call to server shutdown on disable.
-    	      // Won't hurt if server already disables itself, but will prevent plugin unload/reload.
-    	      Bukkit.getLogger().severe(ChatColor.translateAlternateColorCodes('&', "&8-" + getConfig().getString("MessageBeforeStopLoL")));
-    	      Bukkit.shutdown();
+    	      // Set optimised values (soon)
+    	      Bukkit.getLogger().severe(ChatColor.RED + "Nou");
     }
 }
 }}
